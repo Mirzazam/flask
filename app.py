8c2fadfef149c0044132ff608739e89bdc4a599d
mirza <subazam17>
2023-03-13 20:01:12 +0530

    added new bluprints

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-#imporing libraries
-from flask import Flask, Blueprint, render_template, redirect
-#Flask syntax    
+from flask import Flask, Blueprint, render_template
+
+contact_us = Blueprint('contact', __name__, url_prefix='/contact')
+@contact_us.route('/')
+def contact():
+    return render_template('contact-us.html')
+
+payment = Blueprint('payments', __name__, url_prefix='/payments')
+@payment.route('/')
+def pay():
+    return render_template('payments.html')
+
 app = Flask(__name__)
-bp = Blueprint('my_blueprint', __name__)
-
-#creating route for index.html page
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-#creating route for home page
-@app.route('/home')
 def home():
     return render_template('home.html')
 
-app.register_blueprint(bp)
+app.register_blueprint(contact_us)
+app.register_blueprint(payment)
 
-#function to run the app with debug mode which shows the changes made
 app.run(debug=True)
