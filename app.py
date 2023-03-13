3caaaf037c42e038ae38fed6158a9f6e77a42b6c
mirza <subazam17>
2023-03-13 12:37:07 +0530

    added html and css code in home and index page, pulled from internet

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+#imporing libraries
 from flask import Flask, Blueprint, render_template, redirect
+#Flask syntax    
 app = Flask(__name__)
 bp = Blueprint('my_blueprint', __name__)
 
+#creating route for index.html page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@bp.route('/home')
+#creating route for home page
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 app.register_blueprint(bp)
 
+#function to run the app with debug mode which shows the changes made
 app.run(debug=True)
